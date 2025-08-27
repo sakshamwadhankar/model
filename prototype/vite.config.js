6aa92bad7e0ac5a445e1b53df25eb8bd9794e4b6
saksham wadhankar <sakshamwadhankar21@gmail.com>
2025-08-28 02:25:10 +0530

    Update vite.config.js

--- prototype/vite.config.js
+++ prototype/vite.config.js
@@ -4,7 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(() => {
     return {
         build: {
-            outDir: 'build',
+            // ✅ Vercel को "dist" चाहिए, इसलिए यहाँ बदल दिया
+            outDir: 'dist',
         },
         server: {
             open: true,
